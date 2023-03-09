bd3f4604f626cba93b5aef4903873bd55b30de30
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-03-09 15:49:41 -0300

    add new components to the app

--- src/App.js
+++ src/App.js
@@ -8,22 +8,55 @@ import {
     BrowserRouter,
     Routes,
     Route,
-    Link
 } from "react-router-dom";
+import { useEffect, useState } from "react"
+import { services } from "./services"
 
 export default function App() {
+    const [chosenTimeID, setChosenTimeID] = useState('')
+    const [movieShowTime, setMovieShowTime] = useState([])
+    const [selectedSeatInfo, setSelectedSeatsInfo] = useState(null)
+    const [goToSucessPage, setGoToSucessPage] = useState(null)
+    const filteredMovieShowTime = movieShowTime?.days?.reduce((acc, item, i, arr) => {
+        for (let showtime of item.showtimes) {
+            if (showtime.id === Number(chosenTimeID)) {
+                console.log("dentro", item)
+                acc = { date: item.date, movie: movieShowTime.title, time: showtime.name }
+                break;
+            }
+        }
+        return acc
+
+    }, {});
+    
+    useEffect(() => {
+        if (selectedSeatInfo!==null){
+            (async function book(){
+            try {
+                const response = await services.bookSeat({name: selectedSeatInfo.name, cpf: selectedSeatInfo.cpf, ids: selectedSeatInfo.ids})
+                setGoToSucessPage(await response.ok)
+            } catch (e) {
+                console.log(e)
+            } })()
+        }
+    }, [selectedSeatInfo, goToSucessPage])
+
     return (
         <>
             <BrowserRouter>
                 <NavContainer>CINEFLEX</NavContainer>
                 <Routes>
                     <Route path="/" element={<HomePage />}></Route>
-                    <Route path="/sessoes/:id" element={<SessionsPage />}></Route>
-                    <Route path="/assentos/:id" element={<SeatsPage />}></Route>
-                    <Route path="/sucesso" element={<SuccessPage />}></Route>
+                    <Route path="/sessoes/:id" element={movieShowTime && <SessionsPage
+                        movieShowTime={movieShowTime}
+                        setMovieShowTime={setMovieShowTime}
+                        setChosenTimeID={setChosenTimeID} />}></Route>
+                    <Route path="/assentos/:id" element={<SeatsPage setSelectedSeatsInfo={setSelectedSeatsInfo}/>}></Route>
+                    <Route path="/sucesso" element={goToSucessPage && filteredMovieShowTime && selectedSeatInfo && <SuccessPage filteredMovieShowTime={filteredMovieShowTime} selectedSeatInfo={selectedSeatInfo}  /> }></Route>
                 </Routes>
             </BrowserRouter>
         </>
+
     )
 }
 
